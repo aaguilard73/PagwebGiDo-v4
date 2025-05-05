7d0385469ebeb209c2fbd952a714ff57e85c6a78
aaguilard73 <proyectosaad@gmail.com>
2025-05-05 17:10:50 -0600

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,3 +1,5 @@
+// src/data/products.ts
+
 import { Product } from '../types';
 
 export const products: Product[] = [
@@ -8,8 +10,8 @@ export const products: Product[] = [
     line: "arquetipika",
     collection: "arquetipika",
     images: {
-      primary: "https://images.pexels.com/photos/10944556/pexels-photo-10944556.jpeg?auto=compress&cs=tinysrgb&w=800",
-      secondary: "https://images.pexels.com/photos/9974932/pexels-photo-9974932.jpeg?auto=compress&cs=tinysrgb&w=800"
+      primary: "/images/producto1.png",
+      secondary: "/images/producto2.png"
     }
   },
   {
@@ -19,8 +21,8 @@ export const products: Product[] = [
     line: "luminika",
     collection: "luminika",
     images: {
-      primary: "https://images.pexels.com/photos/11638420/pexels-photo-11638420.jpeg?auto=compress&cs=tinysrgb&w=800",
-      secondary: "https://images.pexels.com/photos/10165134/pexels-photo-10165134.jpeg?auto=compress&cs=tinysrgb&w=800"
+      primary: "/images/producto3.png",
+      secondary: "/images/producto4.png"
     }
   },
   {
@@ -30,63 +32,19 @@ export const products: Product[] = [
     line: "antropika",
     collection: "antropika",
     images: {
-      primary: "https://images.pexels.com/photos/10870385/pexels-photo-10870385.jpeg?auto=compress&cs=tinysrgb&w=800",
-      secondary: "https://images.pexels.com/photos/10870384/pexels-photo-10870384.jpeg?auto=compress&cs=tinysrgb&w=800"
+      primary: "/images/producto5.png",
+      secondary: "/images/producto6.png"
     }
   },
   {
     id: 4,
     name: "Anillo Jardín",
     price: 1450,
-    line: "jardins",
-    collection: "jardins",
+    line: "jardinsEtFleurs",
+    collection: "jardinsEtFleurs",
     images: {
-      primary: "https://images.pexels.com/photos/10192396/pexels-photo-10192396.jpeg?auto=compress&cs=tinysrgb&w=800",
-      secondary: "https://images.pexels.com/photos/10192397/pexels-photo-10192397.jpeg?auto=compress&cs=tinysrgb&w=800"
-    }
-  },
-  {
-    id: 5,
-    name: "Pendientes Color",
-    price: 1650,
-    line: "kromatika",
-    collection: "kromatika",
-    images: {
-      primary: "https://images.pexels.com/photos/5370706/pexels-photo-5370706.jpeg?auto=compress&cs=tinysrgb&w=800",
-      secondary: "https://images.pexels.com/photos/5370696/pexels-photo-5370696.jpeg?auto=compress&cs=tinysrgb&w=800"
-    }
-  },
-  {
-    id: 6,
-    name: "Brazalete Escultura",
-    price: 2150,
-    line: "antropika",
-    collection: "antropika",
-    images: {
-      primary: "https://images.pexels.com/photos/17034460/pexels-photo-17034460/free-photo-of-mujer-joya-joyeria-plata.jpeg?auto=compress&cs=tinysrgb&w=800",
-      secondary: "https://images.pexels.com/photos/17034384/pexels-photo-17034384/free-photo-of-mujeres-plata-joyeria-exhibicion.jpeg?auto=compress&cs=tinysrgb&w=800"
-    }
-  },
-  {
-    id: 7,
-    name: "Dije Biwa",
-    price: 1250,
-    line: "arquetipika",
-    collection: "arquetipika",
-    images: {
-      primary: "https://images.pexels.com/photos/10555769/pexels-photo-10555769.jpeg?auto=compress&cs=tinysrgb&w=800",
-      secondary: "https://images.pexels.com/photos/10555771/pexels-photo-10555771.jpeg?auto=compress&cs=tinysrgb&w=800"
-    }
-  },
-  {
-    id: 8,
-    name: "Aretes Cristal",
-    price: 1350,
-    line: "luminika",
-    collection: "luminika",
-    images: {
-      primary: "https://images.pexels.com/photos/14220848/pexels-photo-14220848.jpeg?auto=compress&cs=tinysrgb&w=800",
-      secondary: "https://images.pexels.com/photos/14220847/pexels-photo-14220847.jpeg?auto=compress&cs=tinysrgb&w=800"
+      primary: "/images/producto7.png",
+      secondary: "/images/producto8.png"
     }
   }
 ];
