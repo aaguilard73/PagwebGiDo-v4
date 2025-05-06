d728b327623b61115db3769cb48411590d65bfc0
aaguilard73 <proyectosaad@gmail.com>
2025-05-05 23:08:46 -0600

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -4,11 +4,9 @@ import type { Product } from '../types';
 
 export const products: Product[] = [
   {
-    id: 1,
+    id: 'producto1',
     name: 'Producto 1',
     price: 1000,
-    line: '',
-    collection: '',
     description: 'Descripción breve del producto 1.',
     images: {
       primary: '/images/producto1.png',
@@ -16,11 +14,9 @@ export const products: Product[] = [
     },
   },
   {
-    id: 2,
+    id: 'producto2',
     name: 'Producto 2',
     price: 1200,
-    line: '',
-    collection: '',
     description: 'Descripción breve del producto 2.',
     images: {
       primary: '/images/producto2.png',
@@ -28,11 +24,9 @@ export const products: Product[] = [
     },
   },
   {
-    id: 3,
+    id: 'producto3',
     name: 'Producto 3',
     price: 1400,
-    line: '',
-    collection: '',
     description: 'Descripción breve del producto 3.',
     images: {
       primary: '/images/producto3.png',
@@ -40,11 +34,9 @@ export const products: Product[] = [
     },
   },
   {
-    id: 4,
+    id: 'producto4',
     name: 'Producto 4',
     price: 1600,
-    line: '',
-    collection: '',
     description: 'Descripción breve del producto 4.',
     images: {
       primary: '/images/producto4.png',
@@ -52,11 +44,9 @@ export const products: Product[] = [
     },
   },
   {
-    id: 5,
+    id: 'producto5',
     name: 'Producto 5',
     price: 1800,
-    line: '',
-    collection: '',
     description: 'Descripción breve del producto 5.',
     images: {
       primary: '/images/producto5.png',
@@ -64,11 +54,9 @@ export const products: Product[] = [
     },
   },
   {
-    id: 6,
+    id: 'producto6',
     name: 'Producto 6',
     price: 2000,
-    line: '',
-    collection: '',
     description: 'Descripción breve del producto 6.',
     images: {
       primary: '/images/producto6.png',
@@ -76,11 +64,9 @@ export const products: Product[] = [
     },
   },
   {
-    id: 7,
+    id: 'producto7',
     name: 'Producto 7',
     price: 2200,
-    line: '',
-    collection: '',
     description: 'Descripción breve del producto 7.',
     images: {
       primary: '/images/producto7.png',
@@ -88,11 +74,9 @@ export const products: Product[] = [
     },
   },
   {
-    id: 8,
+    id: 'producto8',
     name: 'Producto 8',
     price: 2400,
-    line: '',
-    collection: '',
     description: 'Descripción breve del producto 8.',
     images: {
       primary: '/images/producto8.png',
