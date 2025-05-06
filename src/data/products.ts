41854d1d6092f4ea1a573e567e60ac949f15d618
aaguilard73 <proyectosaad@gmail.com>
2025-05-06 00:04:39 -0600

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,64 +1,86 @@
-// src/data/products.ts
+import { Product } from '../types';
 
-import type { Product } from '../types';
-
-export const products: Product[] = [
+const products: Product[] = [
   {
     id: 'producto1',
     name: 'Producto 1',
-    price: 1000,
     description: 'Descripción breve del producto 1.',
-    image: '/images/producto1.png',
+    price: 1000,
+    images: {
+      primary: '/images/producto1.png',
+      secondary: '/images/producto1.png'
+    }
   },
   {
     id: 'producto2',
     name: 'Producto 2',
-    price: 1200,
     description: 'Descripción breve del producto 2.',
-    image: '/images/producto2.png',
+    price: 1200,
+    images: {
+      primary: '/images/producto2.png',
+      secondary: '/images/producto2.png'
+    }
   },
   {
     id: 'producto3',
     name: 'Producto 3',
-    price: 1400,
     description: 'Descripción breve del producto 3.',
-    image: '/images/producto3.png',
+    price: 1400,
+    images: {
+      primary: '/images/producto3.png',
+      secondary: '/images/producto3.png'
+    }
   },
   {
     id: 'producto4',
     name: 'Producto 4',
-    price: 1600,
     description: 'Descripción breve del producto 4.',
-    image: '/images/producto4.png',
+    price: 1600,
+    images: {
+      primary: '/images/producto4.png',
+      secondary: '/images/producto4.png'
+    }
   },
   {
     id: 'producto5',
     name: 'Producto 5',
-    price: 1800,
     description: 'Descripción breve del producto 5.',
-    image: '/images/producto5.png',
+    price: 1800,
+    images: {
+      primary: '/images/producto5.png',
+      secondary: '/images/producto5.png'
+    }
   },
   {
     id: 'producto6',
     name: 'Producto 6',
-    price: 2000,
     description: 'Descripción breve del producto 6.',
-    image: '/images/producto6.png',
+    price: 2000,
+    images: {
+      primary: '/images/producto6.png',
+      secondary: '/images/producto6.png'
+    }
   },
   {
     id: 'producto7',
     name: 'Producto 7',
-    price: 2200,
     description: 'Descripción breve del producto 7.',
-    image: '/images/producto7.png',
+    price: 2200,
+    images: {
+      primary: '/images/producto7.png',
+      secondary: '/images/producto7.png'
+    }
   },
   {
     id: 'producto8',
     name: 'Producto 8',
-    price: 2400,
     description: 'Descripción breve del producto 8.',
-    image: '/images/producto8.png',
-  },
+    price: 2400,
+    images: {
+      primary: '/images/producto8.png',
+      secondary: '/images/producto8.png'
+    }
+  }
 ];
 
 export default products;
