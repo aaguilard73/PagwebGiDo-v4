8c046cf71cb4c262fe4cb936729794278457ca47
aaguilard73 <proyectosaad@gmail.com>
2025-05-05 22:37:32 -0600

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -6,74 +6,50 @@ export const products: Product[] = [
   {
     id: 'producto1',
     name: 'Producto 1',
-    price: 1000, // Puedes cambiar el precio que quieras
-    images: {
-      primary: '/images/producto1.png',
-      secondary: '/images/producto1.png',
-    },
+    price: 1000,
+    image: '/images/producto1.png',
   },
   {
     id: 'producto2',
     name: 'Producto 2',
     price: 1200,
-    images: {
-      primary: '/images/producto2.png',
-      secondary: '/images/producto2.png',
-    },
+    image: '/images/producto2.png',
   },
   {
     id: 'producto3',
     name: 'Producto 3',
     price: 1400,
-    images: {
-      primary: '/images/producto3.png',
-      secondary: '/images/producto3.png',
-    },
+    image: '/images/producto3.png',
   },
   {
     id: 'producto4',
     name: 'Producto 4',
     price: 1600,
-    images: {
-      primary: '/images/producto4.png',
-      secondary: '/images/producto4.png',
-    },
+    image: '/images/producto4.png',
   },
   {
     id: 'producto5',
     name: 'Producto 5',
     price: 1800,
-    images: {
-      primary: '/images/producto5.png',
-      secondary: '/images/producto5.png',
-    },
+    image: '/images/producto5.png',
   },
   {
     id: 'producto6',
     name: 'Producto 6',
     price: 2000,
-    images: {
-      primary: '/images/producto6.png',
-      secondary: '/images/producto6.png',
-    },
+    image: '/images/producto6.png',
   },
   {
     id: 'producto7',
     name: 'Producto 7',
     price: 2200,
-    images: {
-      primary: '/images/producto7.png',
-      secondary: '/images/producto7.png',
-    },
+    image: '/images/producto7.png',
   },
   {
     id: 'producto8',
     name: 'Producto 8',
     price: 2400,
-    images: {
-      primary: '/images/producto8.png',
-      secondary: '/images/producto8.png',
-    },
+    image: '/images/producto8.png',
   },
 ];
 
