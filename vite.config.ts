7d9be131260a3f7792623fab612a4912a753f7cb
aaguilard73 <proyectosaad@gmail.com>
2025-05-07 21:41:00 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // Para que todas las rutas de assets sean relativas
   base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   build: {
-    outDir: 'dist',      // Salida en dist/
-    emptyOutDir: true,   // Limpia dist/ en cada build
-    assetsDir: 'assets', // Archivos (js, css, imágenes) en dist/assets/
+    outDir: 'dist',
+    emptyOutDir: true,
+    assetsDir: 'assets',
   },
   resolve: {
     alias: {
-      '@public': '/public',
-      '@images': '/public/images', // Añadimos este alias
+      '@public': '/public', // Mantén este alias si lo usas para otras cosas
     },
   },
-  publicDir: 'public',
 });
