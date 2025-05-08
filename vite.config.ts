c0d1d823cd9a315537e43cffeb45fd082592fc64
aaguilard73 <proyectosaad@gmail.com>
2025-05-07 20:03:04 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   // Para que todas las rutas de assets sean relativas
-  base: './',
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   build: {
-    outDir: 'dist',       // Salida en dist/
-    emptyOutDir: true,    // Limpia dist/ en cada build
-    assetsDir: 'assets',  // Archivos (js, css, imágenes) en dist/assets/
+    outDir: 'dist',      // Salida en dist/
+    emptyOutDir: true,   // Limpia dist/ en cada build
+    assetsDir: 'assets', // Archivos (js, css, imágenes) en dist/assets/
   },
   resolve: {
     alias: {
